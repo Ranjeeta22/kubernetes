ef40d96f522dcf475d83300a8a90dd96329afa0e
mvpc-11232 <mvp2612@hotmail.com>
2022-09-26 22:35:52 +0530

    pushing EKS

--- eks/data.tf
+++ eks/data.tf
@@ -3,7 +3,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
     bucket = "b49-rf-remote-state-bucket"
-    key    = "vpc/${var.ENV}/terraform.tfstate"
+    key    = "vpc/dev/terraform.tfstate"
     region = "us-east-1"
   }
 }
