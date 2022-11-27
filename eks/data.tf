03c12a8d8b123aac35ee19931d0b9e133d0688f9
mvpc-11232 <mvp2612@hotmail.com>
2022-11-27 23:56:20 +0530

    asdf

--- eks/data.tf
+++ eks/data.tf
@@ -1,9 +1,8 @@
-# Using this remote data source, we are fetching the outputs from remote state file. in this case VPC Statefile
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
     bucket = "b50-tf-state-bucket"
-    key    = "vpc/dev/terraform.tfstate"
+    key    = "vpc/${var.ENV}/terrafom.tfstate"
     region = "us-east-1"
   }
 }
